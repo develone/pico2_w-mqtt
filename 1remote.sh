d69c650b1a8ae3273136fa4595b21aede9854f9a
Edward Vidal Jr <develone@sbcglobal.net>
2025-04-05 09:53:06 -0600

    -DPICO_BOARD=pico_w -> -DPICO_BOARD=pico2_w

--- 1remote.sh
+++ 1remote.sh
@@ -10,7 +10,7 @@ cd remote5
 
 cp ../exe-ocd.sh .
 
-cmake -DPICO_BOARD=pico_w  -DHOSTNAME="remote5" -DWIFI_SSID="WIFI_SSID" -DWIFI_PASSWORD="WIFI_PASSWORD" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
+cmake -DPICO_BOARD=pico2_w  -DHOSTNAME="remote5" -DWIFI_SSID="WIFI_SSID" -DWIFI_PASSWORD="WIFI_PASSWORD" -DFREERTOS_KERNEL_PATH:PATH=/home/devel/FreeRTOS-Kernel ..
 
 
 make
