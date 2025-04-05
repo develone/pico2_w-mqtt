5f3212d89d52916b6de82e912dbd454c535061c4
Edward Vidal Jr <develone@sbcglobal.net>
2025-04-05 09:25:51 -0600

    these are required in .bashrc export PICO_SDK_PATH=/home/devel/pico2/pico-sdk export FREERTOS_KERNEL_PATH=/home/devel/pico2/freertos-lib/FreeRTOS-Kernel export PICTOOL_FETCH_FROM_GITHUB=/home/devel/pico2/BUILT_PICOTOOL pico_w folder from pi4-60 to pico2_2 pico2_w-pico_w.txt is tree of pico2_w

---
+++ pico2_w/wifi/freertos/iperf/mqtt_extra.h
@@ -0,0 +1,39 @@
+#ifndef MQTT_EXTRA_H
+#define MQTT_EXTRA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "hardware/watchdog.h"
+#include "pico/time.h"
+//void mqtt_example_init(void);
+//#endif
+
+/**************************tcp_server**************************/
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
+
+// mqtt_client_t* saved_mqtt_client = NULL;
+ u8_t mqtt_connected = 1;
+ u8_t check_mqtt_connected;
+
+ u8_t check_wifi_connected;
+ u8_t wifi_connected = 1;
+
+char ver[7] = "0.0.11";
+#define debug_level 
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MQTT_EXTRA_H */
